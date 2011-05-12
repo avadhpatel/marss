f8c4f44709b4e65bd6bcc9b51c9a0fb02c6e1860
Avadh Patel <avadh4all@gmail.com>
2011-05-12 09:56:01 -0700

    Fix bug in timer alarm setup at init
    This fix allows timer alarm to be set before we switch to simulation.
    
    Signed-off-by: Avadh Patel <avadh4all@gmail.com>

--- qemu/vl.c
+++ qemu/vl.c
@@ -1441,7 +1441,7 @@ static void main_loop(void)
 #endif
 #ifndef CONFIG_IOTHREAD
 #ifdef MARSS_QEMU
-            if(in_simulation) {
+            if(in_simulation && !qemu_alarm_pending()) {
                 /* cur_cpu = first_cpu; */
                 sim_cpu_exec();
             } else {
