cd8f1d1a70724d22be09a0b56022f2f77e1375ef
Avadh Patel <avadh4all@gmail.com>
2012-04-03 08:24:25 -0700

    Added 'likely/unlikely' to cpuController and memoryHierarchy
    Signed-off-by: Avadh Patel <avadh4all@gmail.com>

--- ptlsim/cache/memoryHierarchy.cpp
+++ ptlsim/cache/memoryHierarchy.cpp
@@ -401,7 +401,7 @@ bool MemoryHierarchy::grab_lock(W64 lockaddr, W8 ctx_id)
     bool ret = false;
     MemoryInterlockEntry* lock = interlocks.select_and_lock(lockaddr);
 
-    if(lock && lock->ctx_id == (W8)-1) {
+    if likely (lock && lock->ctx_id == (W8)-1) {
         lock->ctx_id = ctx_id;
         ret = true;
     }
@@ -437,7 +437,7 @@ bool MemoryHierarchy::probe_lock(W64 lockaddr, W8 ctx_id)
     bool ret = false;
     MemoryInterlockEntry* lock = interlocks.probe(lockaddr);
 
-    if(!lock) { // If no one has grab the lock
+    if likely (!lock) { // If no one has grab the lock
         ret = true;
     } else if(lock && lock->ctx_id == ctx_id) {
         ret = true;
